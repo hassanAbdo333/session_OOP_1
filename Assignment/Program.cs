7d75d15286df1aa7089e22a81e7fd868f560201e
hassankozo333@gmail.com <hassankozo333@gmail.com>
2025-01-18 00:55:00 +0200

    first commit

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignment
+﻿using System.ComponentModel;
+
+namespace Assignment
 {
     #region p1
     enum WeekDays
@@ -30,146 +32,151 @@
         private static void Main()
         {
             #region p01
+
+            //Create an enum called "WeekDays" with the days of the week(Monday to Sunday)
+            //as its members.Then, write a C# program that prints out all the days of the week using this enum.
+
             //foreach(WeekDays Day in Enum.GetValues(typeof(WeekDays)))
             //{
             //    Console.WriteLine(Day);
 
-            //}
-            #endregion
-            #region p02
-            //Person[] people = new Person[3] ;
-            //people[0] = new Person("hassan", 22);
-            //people[1] = new Person("abdo", 29);
-            //people[2] = new Person("hassan", 32);
+        //}
+        #endregion
+        #region p02
+        //Person[] people = new Person[3] ;
+        //people[0] = new Person("hassan", 22);
+        //people[1] = new Person("abdo", 29);
+        //people[2] = new Person("hassan", 32);
 
-            //foreach(Person person in people)
-            //{
-            //    Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
-            //}
+        //foreach(Person person in people)
+        //{
+        //    Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+        //}
 
-            #endregion
-            #region p03
-            //Console.WriteLine("Enter a season (Spring, Summer, Autumn, Winter):");
-            //string input = Console.ReadLine();
-            //Season season = (Season)Enum.Parse(typeof(Season), input,true);
+        #endregion
+        #region p03
+        //Console.WriteLine("Enter a season (Spring, Summer, Autumn, Winter):");
+        //string input = Console.ReadLine();
+        //Season season = (Season)Enum.Parse(typeof(Season), input,true);
 
-            //if (season == Season.Spring)
-            //{
-            //    Console.WriteLine("Spring: March to May");
-            //}
-            //else if (season == Season.Summer)
-            //{
-            //    Console.WriteLine("Summer: June to August");
-            //}
-            //else if(season == Season.Autumn)
-            //{
-            //    Console.WriteLine("Autumn: September to November");
-            //}
-            //else if(season == Season.Winter)
-            //{
-            //    Console.WriteLine("Winter: December to February");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Invalid season entered.");
-            //}
+        //if (season == Season.Spring)
+        //{
+        //    Console.WriteLine("Spring: March to May");
+        //}
+        //else if (season == Season.Summer)
+        //{
+        //    Console.WriteLine("Summer: June to August");
+        //}
+        //else if(season == Season.Autumn)
+        //{
+        //    Console.WriteLine("Autumn: September to November");
+        //}
+        //else if(season == Season.Winter)
+        //{
+        //    Console.WriteLine("Winter: December to February");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Invalid season entered.");
+        //}
 
 
-            //switch (season)
-            //{
-            //    case Season.Spring:
-            //        Console.WriteLine("Spring: March to May");
-            //        break;
-            //    case Season.Summer:
-            //        Console.WriteLine("Summer: June to August");
-            //        break;
-            //    case Season.Autumn:
-            //        Console.WriteLine("Autumn: September to November");
-            //        break;
-            //    case Season.Winter:
-            //        Console.WriteLine("Winter: December to February");
-            //        break;
-            //    default:
-            //        Console.WriteLine("Invalid season entered.");
-            //        break;
-            //}
+        //switch (season)
+        //{
+        //    case Season.Spring:
+        //        Console.WriteLine("Spring: March to May");
+        //        break;
+        //    case Season.Summer:
+        //        Console.WriteLine("Summer: June to August");
+        //        break;
+        //    case Season.Autumn:
+        //        Console.WriteLine("Autumn: September to November");
+        //        break;
+        //    case Season.Winter:
+        //        Console.WriteLine("Winter: December to February");
+        //        break;
+        //    default:
+        //        Console.WriteLine("Invalid season entered.");
+        //        break;
+        //}
 
-            #endregion
-            #region p04
-            //Permission userPermissions = Permission.None;
+        #endregion
+        #region p04
+        //Permission userPermissions = Permission.None;
 
-            //userPermissions |= Permission.Read;  
-            //userPermissions |= Permission.Write;  
-            //userPermissions |= Permission.Execute; 
-            //Console.WriteLine("Current Permissions: " + userPermissions);
+        //userPermissions |= Permission.Read;  
+        //userPermissions |= Permission.Write;  
+        //userPermissions |= Permission.Execute; 
+        //Console.WriteLine("Current Permissions: " + userPermissions);
 
-            //bool hasReadPermission = (userPermissions & Permission.Read) == Permission.Read;
-            //bool hasDeletePermission = (userPermissions & Permission.Delete) == Permission.Delete;
-            //Console.WriteLine("Has Read Permission: " + hasReadPermission);
-            //Console.WriteLine("Has Delete Permission: " + hasDeletePermission);
+        //bool hasReadPermission = (userPermissions & Permission.Read) == Permission.Read;
+        //bool hasDeletePermission = (userPermissions & Permission.Delete) == Permission.Delete;
+        //Console.WriteLine("Has Read Permission: " + hasReadPermission);
+        //Console.WriteLine("Has Delete Permission: " + hasDeletePermission);
 
-            //userPermissions &= ~Permission.Write;
-            //Console.WriteLine("Updated Permissions (after removing Write): " + userPermissions);
+        //userPermissions &= ~Permission.Write;
+        //Console.WriteLine("Updated Permissions (after removing Write): " + userPermissions);
 
-            //bool hasWritePermission = (userPermissions & Permission.Write) == Permission.Write;
-            //Console.WriteLine("Has Write Permission: " + hasWritePermission);
-            #endregion
-            #region p05
-            //Console.WriteLine("Enter a color:");
-            //string input = Console.ReadLine();
-            //try 
-            //{ 
-            //Colors color = (Colors)Enum.Parse(typeof(Colors), input, true);
-            //    Console.WriteLine($"{color} is a primary color.");
-            //}
-            //catch (ArgumentException)
-            //{
-            //    Console.WriteLine($"{input} is not a primary color.");
-            //}
-            #endregion
-            #region p06
-            //Console.WriteLine("Enter the first point (X and Y):");
-            //double x1;
-            //double.TryParse(Console.ReadLine(), out x1);
-            //double y1;
-            //double.TryParse(Console.ReadLine(), out y1);
-            //Console.WriteLine("Enter the  second point (X and Y):");
-            //double x2;
-            //double.TryParse(Console.ReadLine(), out x2);
-            //double y2;
-            //double.TryParse(Console.ReadLine(), out y2);
+        //bool hasWritePermission = (userPermissions & Permission.Write) == Permission.Write;
+        //Console.WriteLine("Has Write Permission: " + hasWritePermission);
+        #endregion
+        #region p05
+        //Console.WriteLine("Enter a color:");
+        //string input = Console.ReadLine();
+        //try 
+        //{ 
+        //Colors color = (Colors)Enum.Parse(typeof(Colors), input, true);
+        //    Console.WriteLine($"{color} is a primary color.");
+        //}
+        //catch (ArgumentException)
+        //{
+        //    Console.WriteLine($"{input} is not a primary color.");
+        //}
+        #endregion
+        #region p06
+        //Console.WriteLine("Enter the first point (X and Y):");
+        //double x1;
+        //double.TryParse(Console.ReadLine(), out x1);
+        //double y1;
+        //double.TryParse(Console.ReadLine(), out y1);
+        //Console.WriteLine("Enter the  second point (X and Y):");
+        //double x2;
+        //double.TryParse(Console.ReadLine(), out x2);
+        //double y2;
+        //double.TryParse(Console.ReadLine(), out y2);
 
-            //point p1 = new point(x1,y1);
-            //point p2 = new point(x2,y2);
-            //double distance = Math.Sqrt(Math.Pow(p2.x - p1.x, 2) + Math.Pow(p2.y - p1.y, 2));
-            //Console.WriteLine($"The distance between the two points is: {distance}");
-            #endregion
-            #region p07
-            //Person[] persons = new Person[3];
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    Console.WriteLine($"Enter details for Person {i + 1}:");
+        //point p1 = new point(x1,y1);
+        //point p2 = new point(x2,y2);
+        //double distance = Math.Sqrt(Math.Pow(p2.x - p1.x, 2) + Math.Pow(p2.y - p1.y, 2));
+        //Console.WriteLine($"The distance between the two points is: {distance}");
+        #endregion
+        #region p07
+        //Person[] persons = new Person[3];
+        //for (int i = 0; i < 3; i++)
+        //{
+        //    Console.WriteLine($"Enter details for Person {i + 1}:");
 
-            //    Console.Write("Name: ");
-            //    string name = Console.ReadLine();
+        //    Console.Write("Name: ");
+        //    string name = Console.ReadLine();
 
-            //    Console.Write("Age: ");
-            //    int age ;
-            //    int.TryParse(Console.ReadLine(), out age);
+        //    Console.Write("Age: ");
+        //    int age ;
+        //    int.TryParse(Console.ReadLine(), out age);
 
-            //    persons[i] = new Person(name, age);
-            //}
-            //Person oldest = persons[0];
-            //foreach (var person in persons)
-            //{
-            //    if (person.Age > oldest.Age)
-            //    {
-            //        oldest = person;
-            //    }
-            //}
+        //    persons[i] = new Person(name, age);
+        //}
+        //Person oldest = persons[0];
+        //foreach (var person in persons)
+        //{
+        //    if (person.Age > oldest.Age)
+        //    {
+        //        oldest = person;
+        //    }
+        //}
 
-            //Console.WriteLine(oldest);
-            #endregion
+        //Console.WriteLine(oldest);
+        #endregion
+
 
         }
     }
